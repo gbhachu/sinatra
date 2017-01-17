23585fbdf0772db4af4f7b87b0f7c6f5062e00b2
Paul Goodman <paulyjgooders@gmail.com>
2017-01-17 15:52:53 +0000

    recent changes

--- app.rb
+++ app.rb
@@ -14,16 +14,16 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
-  p params
-  @name = params[:name]
-  erb(:index)
-end
 
 post '/named-cat' do
   p params
   @name = params[:name]
   erb(:index)
+end
+
+
+get '/cat-form' do
+  erb (:cat_form)
 end
 
 set :session_secret, 'super secret'
